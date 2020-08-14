500f1bd41cbeba9afe70376a30f3c9e686f3dda0
akuto aoyama <aaaa.akuto@gmail.com>
2020-08-14 23:55:34 +0900

    [add]push,findを使って文字列化成功

--- node/index.js
+++ node/index.js
@@ -2,11 +2,6 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-   // url = new URL(req.url)
-   // console.log('url.search', url.searchreq)
-   // console.log('url.hash', url.hash)
-
-
    const endpoint = req.url;
    if( endpoint==='/start' ){
       fs.readFile('./index.html',(err, data)=>{
@@ -16,20 +11,29 @@ const server = http.createServer((req, res) => {
       });
    }
    if (endpoint === '/api') {
-      console.info('req.method', req.method)
-      console.info('req.query',req.query)
-      
       if (req.method == 'POST') {
-         console.info('POST')
          var body = ''
          req.on('data', function (data) {
             body += data
-            console.info('Partial body: ' + body)
          })
-         req.on('end', function() {
-            console.log(body);
+         req.on('end', function () {
+            var result = [];
+
+            for (var i = 1; i < 31; i++) {
+               const list = JSON.parse(body).obj.reverse();
+               const found = list.find(element => i % element.num === 0);
+               if (found !== undefined) {
+                  result.push(found.text);
+               }else{
+                  result.push(i.toString());
+               }
+            }
+            resultList = result.join(', ');
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            const responseData = {data: body}
+
+            body = JSON.stringify(resultList).replace(/[\"]/g,"")
+            
+            const responseData = { data: body }
             res.write(JSON.stringify(responseData));
             res.end();
          });

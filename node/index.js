7b51ba44660804e29b594e790f0404622129461a
akuto aoyama <aaaa.akuto@gmail.com>
2020-08-14 18:34:58 +0900

    [add]ブラウザへの出力成功

--- node/index.js
+++ node/index.js
@@ -1,7 +1,12 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
+   // url = new URL(req.url)
+   // console.log('url.search', url.searchreq)
+   // console.log('url.hash', url.hash)
+
+
    const endpoint = req.url;
    if( endpoint==='/start' ){
       fs.readFile('./index.html',(err, data)=>{
@@ -10,8 +15,25 @@ const server = http.createServer((req, res)=>{
          res.end();
       });
    }
-   if( endpoint==='/api' ){
-      // ここに処理を記述してください。  
+   if (endpoint === '/api') {
+      console.info('req.method', req.method)
+      console.info('req.query',req.query)
+      
+      if (req.method == 'POST') {
+         console.info('POST')
+         var body = ''
+         req.on('data', function (data) {
+            body += data
+            console.info('Partial body: ' + body)
+         })
+         req.on('end', function() {
+            console.log(body);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            const responseData = {data: body}
+            res.write(JSON.stringify(responseData));
+            res.end();
+         });
+      }
    }
 });
 server.listen(8080); 
